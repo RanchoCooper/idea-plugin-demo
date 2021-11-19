3912953671b91f4fbbd417ef6eff5dd9abe18b0e
GitHub Action <action@github.com>
2021-11-19 23:39:23 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ranchocooper/ideaplugindemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ranchocooper.ideaplugindemo.services
+
+import com.github.ranchocooper.ideaplugindemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
